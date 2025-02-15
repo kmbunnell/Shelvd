01734c7c835ebd665bbeff943a627f8e1d23b3bc
Kristina Bunnell <mrskbunnell@gmail.com>
2025-02-15 15:48:07 -0500

    Add book to shelf

--- app/src/main/java/com/shelvd/ui/screens/ScanBook/ScanBookVm.kt
+++ app/src/main/java/com/shelvd/ui/screens/ScanBook/ScanBookVm.kt
@@ -8,6 +8,7 @@ import com.shelvd.data.model.IsbnScanner
 import com.shelvd.data.model.ShelvedBook
 import com.shelvd.domain.IsbnLookUpUseCase
 import com.shelvd.domain.ScanBookUseCase
+import com.shelvd.domain.ShelveBookUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,8 +16,11 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ScanBookVm @Inject constructor(private val scanBookUseCase: ScanBookUseCase,
-                                     private val isbnLookUpUseCase: IsbnLookUpUseCase): ViewModel() {
+class ScanBookVm @Inject constructor(
+    private val scanBookUseCase: ScanBookUseCase,
+    private val isbnLookUpUseCase: IsbnLookUpUseCase,
+    private val shelveBookUseCase: ShelveBookUseCase
+) : ViewModel() {
 
     private val _state = MutableStateFlow<ScanBookViewState>(ScanBookViewState.AwaitScan)
     val state: StateFlow<ScanBookViewState> = _state
@@ -27,35 +31,41 @@ class ScanBookVm @Inject constructor(private val scanBookUseCase: ScanBookUseCas
                 scan()
             }
 
-            is ScanBookIntent.LookUp->{
+            is ScanBookIntent.LookUp -> {
                 lookUp(intent.isbn)
             }
-            is ScanBookIntent.ShelveBook->{
-                val shelf = intent.shelf
+
+            is ScanBookIntent.ShelveBook -> {
+                shelveBook(intent.book.copy(shelf = intent.shelf))
             }
+        }
+    }
+    private fun shelveBook(book:ShelvedBook)
+    {
+        viewModelScope.launch {
+            shelveBookUseCase.invoke(book)
         }
     }
 
     private fun scan() {
         viewModelScope.launch {
-            scanBookUseCase.invoke().collect{
-                result -> updateUIState(result)
+            scanBookUseCase.invoke().collect { result ->
+                updateUIState(result)
 
             }
         }
     }
 
-    private fun lookUp(isbn: String){
+    private fun lookUp(isbn: String) {
         viewModelScope.launch {
-            isbnLookUpUseCase.invoke(isbn).collect{
-                result -> updateUIState(result)
+            isbnLookUpUseCase.invoke(isbn).collect { result ->
+                updateUIState(result)
             }
         }
     }
 
-    private fun updateUIState(book: ShelvedBook?)
-    {
-        if(book!=null)
+    private fun updateUIState(book: ShelvedBook?) {
+        if (book != null)
             _state.value = ScanBookViewState.BookScanSuccess(book)
         else
             _state.value = ScanBookViewState.BookScanError("Isbn look up failed")
