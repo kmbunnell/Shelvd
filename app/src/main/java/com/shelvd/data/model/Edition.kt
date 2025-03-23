e641c997482ca4af5865d50b94117129c0db19f4
Kristina Bunnell <mrskbunnell@gmail.com>
2025-03-23 11:38:02 -0400

    Create enum and enumset to track edition and notes field for shelved book

---
+++ app/src/main/java/com/shelvd/data/model/Edition.kt
@@ -0,0 +1,9 @@
+package com.shelvd.data.model
+
+enum class Edition {
+    HARDBACK,
+    PAPERBACK,
+    ARC,
+    SPECIAL,
+    SIGNED,
+}
--- app/src/main/java/com/shelvd/data/model/ShelvedBook.kt
+++ app/src/main/java/com/shelvd/data/model/ShelvedBook.kt
@@ -1,11 +1,34 @@
 package com.shelvd.data.model
 
+import java.util.EnumSet
+
+
 data class ShelvedBook(
     val authors: List<String>,
     val title: String,
     val isbn: String = "",
-    val shelf: Shelf = Shelf.WANT
-)
+    val shelf: Shelf = Shelf.WANT,
+){
+    val editionSet: EnumSet<Edition> = EnumSet.noneOf(Edition::class.java)
+    var _notes: String=""
+
+
+    @Synchronized
+    fun addEdition(edition: Edition) {
+        editionSet.add(edition)
+    }
+
+    @Synchronized
+    fun removeEdition(edition: Edition) {
+        editionSet.remove(edition)
+    }
+
+    @Synchronized
+    fun getEditions(): Set<Edition> {
+        return editionSet.toSet()
+    }
+
+}
 
 
 
